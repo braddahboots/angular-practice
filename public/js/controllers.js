74a89779996c08bdf9f46d319ba7df0f93b5c6b2
BraddahBoots <bryan@thebrocode.io>
2015-10-31 12:03:42 -1000

    created a value for controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,8 @@ var myApp = angular.module('myApp');
 
 // what ever the dependency is, angular will inject it within the function argument
 // the 2nd argumnet that is passed through controller is an array made of string values that represent the dependencies within the function callback
-myApp.controller('myController', ['$scope', function($scope) {
+myApp.controller('myController', ['$scope', 'mainCharacter', function($scope, mainCharacter) {
   $scope.myFirstName = 'Boots';
   $scope.myModel = 'Ready Player One';
+  $scope.mainCharacter = mainCharacter;
 }]);
