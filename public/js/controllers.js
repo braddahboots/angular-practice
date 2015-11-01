ea0c68a2b6a82a8b88e8644d03a52fe30a0369e5
BraddahBoots <bryan@thebrocode.io>
2015-10-31 14:03:14 -1000

    created factories module

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,8 +2,9 @@ var myApp = angular.module('myApp');
 
 // what ever the dependency is, angular will inject it within the function argument
 // the 2nd argumnet that is passed through controller is an array made of string values that represent the dependencies within the function callback
-myApp.controller('myController', ['$scope', 'mainCharacter', function($scope, mainCharacter) {
+myApp.controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', function($scope, mainCharacter, CharacterVersionFactory) {
   $scope.myFirstName = 'Boots';
   $scope.myModel = 'Ready Player One';
   $scope.mainCharacter = mainCharacter;
+  $scope.characterVersion = CharacterVersionFactory.characterVersion();
 }]);
---
+++ public/js/factories.js
@@ -0,0 +1,10 @@
+angular.module('myApp')
+.factory('CharacterVersionFactory', ['APP_VERSION', 'mainCharacter', function(APP_VERSION, mainCharacter) {
+
+  return {
+    characterVersion : function() {
+      return mainCharacter + ' ' + APP_VERSION;
+    }
+  };
+
+}]);
