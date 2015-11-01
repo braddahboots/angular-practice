61bb8098fede8d4fbaf147722c6d3a7c8c784321
BraddahBoots <bryan@thebrocode.io>
2015-10-31 14:20:36 -1000

    created a services file

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,9 +2,17 @@ var myApp = angular.module('myApp');
 
 // what ever the dependency is, angular will inject it within the function argument
 // the 2nd argumnet that is passed through controller is an array made of string values that represent the dependencies within the function callback
-myApp.controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', function($scope, mainCharacter, CharacterVersionFactory) {
+myApp.controller('myController', [
+  '$scope',
+  'mainCharacter',
+  'CharacterVersionFactory',
+  'BookService',
+  function($scope, mainCharacter, CharacterVersionFactory, BookService) {
+
   $scope.myFirstName = 'Boots';
   $scope.myModel = 'Ready Player One';
   $scope.mainCharacter = mainCharacter;
   $scope.characterVersion = CharacterVersionFactory.characterVersion();
+  $scope.books = BookService.getBooks();
+
 }]);
---
+++ public/js/services.js
@@ -0,0 +1,71 @@
+angular.module('myApp')
+  .service('BookService', [function() {
+
+    var books = [
+      {
+        title : 'Talion the Revenant',
+        author : 'Michael A. Stackpole'
+      },
+      {
+        title : 'All my friends are dead',
+        author : 'Avery Monsen & Jory John'
+      },
+      {
+        title : 'Unbreakable Lightness of being',
+        author : 'Milan Kunder'
+      },
+      {
+        title : 'Eloquent Javascript',
+        author : 'Marijin Haverbeke'
+      },
+      {
+        title : 'A New Earh',
+        authro : 'Eckhart Tolle'
+      },
+      {
+        title : 'Tuesdays with Morrie',
+        author : 'Mitch Album'
+      },
+      {
+        title : 'Outliers',
+        author : 'Malcolm Gladwell'
+      },
+      {
+        title : 'B is for Beer',
+        author : 'Tom Robbins'
+      }
+    ];
+
+    this.getBook = function(id) {
+
+
+    };
+
+    this.getBooks = function() {
+      return books;
+
+    };
+
+
+  }])
+  .service('MagazineService', MagazineService)
+  .service('VideoService', ['$http', VideoService]);
+
+
+
+  function myClass(args1, args2) {
+    this.myProp = 1;
+  }
+
+  function MagazineService() {
+    this.getMagazine = function(id) {
+
+    };
+    this.totalMagazines = 20;
+  }
+
+  function VideoService($http) {
+    this.getVideos = function() {
+
+    };
+  }
